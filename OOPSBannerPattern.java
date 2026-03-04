e1c42422a2ec2c298a25d9cc6042f1b4f6866e28
kasipavan2007 <kasipavan2007@gmail.com>
2026-03-04 18:06:46 +0530

    UC1-Print OOPS

---
+++ OOPSBannerPattern.java
@@ -0,0 +1,13 @@
+public class OOPSBannerPattern {
+	pubic static void main(String[] args){
+		System.out.println("   ***   "+"   ***   "+"********	"+"   *****");
+		System.out.println(" **   ** "+" **   **  "+"**     **   "+" **      ");
+		System.out.println("**     **"+"**     ** "+"**      **  "+"**       ");
+		System.out.println("**     **"+"**     ** "+"**     **   "+" **      ");
+		System.out.println("**	   **"+"**     ** "+"*******           ***   ");
+		System.out.println("**     **"+"**     ** "+"**          "+"      ** ");
+		System.out.println("**     **"+"**     ** "+"**          "+"       **");
+		System.out.println(" **   ** "+" **   **  "+"**          "+"      ** ");
+		System.out.println("   ***   "+"   ***    "+"**          "+" *****   ");
+		}
+		}
